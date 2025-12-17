0a5ee2fdd8f5674abae553ddd530e9bb74451eec
Silmoon <silmoon.hossain@city.ac.uk>
2025-12-17 21:39:10 +0000

    fixed gap size, ground size, made adjustable gap size min and max

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -4,17 +4,33 @@ public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float deadZone = -35;
-    public float gapsize = 5;
+    public float gapsize = 10f;
 
+    private Transform topPipe;
+    private Transform bottomPipe;
+    private bool gapApplied = false;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        topPipe = transform.Find("TopPipe");
+        bottomPipe = transform.Find("BottomPipe");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!gapApplied)
+        {
+            topPipe.localPosition = new Vector3(0, gapsize, 0);
+            bottomPipe.localPosition = new Vector3(0, -gapsize, 0);
+            gapApplied = true;
+            Debug.Log($"TopPipe Y: {topPipe.localPosition.y}, BottomPipe Y: {bottomPipe.localPosition.y}, gapsize: {gapsize}");
+
+        }
         transform.position += Vector3.left * (moveSpeed  * Time.deltaTime);
         if (transform.position.x < deadZone)
         {
--- Assets/PipeSpawnerScript.cs
+++ Assets/PipeSpawnerScript.cs
@@ -6,7 +6,9 @@ public class PipeSpawnerScript : MonoBehaviour
     public float spawnRate = 1;
     private float timer = 0;
     public float heightOffset = 10;
-    
+    public float minFlyableGap = 8f;
+    public float maxFlyableGap = 11f;
+    private float pipeHalfHeight = 25f;
     
     void Start()
     {
@@ -31,6 +33,11 @@ public class PipeSpawnerScript : MonoBehaviour
     {
         float lowestPoint = transform.position.y - heightOffset;
         float highestPoint = transform.position.y + heightOffset;
-        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+        GameObject newPipe = Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+
+        float flyableGap = Random.Range(minFlyableGap, maxFlyableGap);
+        float gapFromCenter = (flyableGap / 2) + pipeHalfHeight;
+        
+        newPipe.GetComponent<PipeMoveScript>().gapsize = gapFromCenter;
     }
 }
