3b492515de653763ebc922060cd13221bef13710
Silmoon <silmoon.hossain@city.ac.uk>
2025-12-18 01:10:18 +0000

    made ground handling less repetitive

--- Assets/GroundScroller.cs
+++ Assets/GroundScroller.cs
@@ -3,9 +3,7 @@ using UnityEngine;
 public class GroundScroller : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 11f;
-    private Transform ground1;
-    private Transform ground2;
-    private Transform ground3;
+    private Transform[] groundSegments;
 
     private float groundHalfWidth = 48.73f/2;
     private float resetThreshold;
@@ -15,11 +13,14 @@ public class GroundScroller : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ground1 = transform.Find("Ground1");
-        ground2 = transform.Find("Ground2");
-        ground3 = transform.Find("Ground3");
+        groundSegments = new Transform[]
+        {
+            transform.Find("Ground1"),
+            transform.Find("Ground2"),
+            transform.Find("Ground3")
+        };
 
-        float spacing = ground3.position.x - ground2.position.x;
+        float spacing = groundSegments[2].position.x - groundSegments[1].position.x;
         teleportationDistance = spacing * 3;
         resetThreshold = -Camera.main.orthographicSize * Camera.main.aspect;
 
@@ -28,29 +29,17 @@ public class GroundScroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // moving entire parent
-        ground1.position += Vector3.left * (moveSpeed  * Time.deltaTime);
-        ground2.position += Vector3.left * (moveSpeed  * Time.deltaTime);
-        ground3.position += Vector3.left * (moveSpeed  * Time.deltaTime);
-        
-        
-        float ground1RightEdge = ground1.position.x + groundHalfWidth;
-        if (ground1RightEdge < resetThreshold)
+        foreach (var segment in groundSegments)
         {
-            ground1.position += new Vector3(teleportationDistance, 0, 0);
+            segment.position += Vector3.left * (moveSpeed * Time.deltaTime);
+
+            float segmentRightEdge = segment.position.x + groundHalfWidth;
+            if (segmentRightEdge < resetThreshold)
+            {
+                segment.position += new Vector3(teleportationDistance, 0, 0);
+            }
         }
-        
-        float ground2RightEdge = ground2.position.x + groundHalfWidth;
-        if (ground2RightEdge < resetThreshold)
-        {
-            ground2.position += new Vector3(teleportationDistance, 0, 0);
-        }
-        float ground3RightEdge = ground3.position.x + groundHalfWidth;
-        if (ground3RightEdge < resetThreshold)
-        {
-            ground3.position += new Vector3(teleportationDistance, 0, 0);
-        }
-        
+
 
     }
 }
